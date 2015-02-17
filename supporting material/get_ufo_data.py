8591cb5c9130311a44e9345b38ccae79ab4d1476
Josiah Davis <josiah.j.davis@gmail.com>
2015-02-17 17:40:19 -0500

    Removed some unused code

--- supporting material/get_ufo_data.py
+++ supporting material/get_ufo_data.py
@@ -1,36 +1,38 @@
-"""
+'''
 Get Data from the National UFO Reporting Center
-
-Modified from example by Hideki Fujioka 
-http://www.ccs.tulane.edu/~fuji/DataVisualization14/GetUFOReport.py
-"""
+'''
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
 # Create list of states to loop through
-#states = ['va', 'mo', 'az', 'pa', 'tx']
-
 states =['AL','AK','AZ', 'AR','CA','CO','CT','DE','FL','GA','HI','ID','IL',
          'IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT',
          'NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI',
          'SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY']
          
-# Initialize dataframe
+# Initialize dataframe to store results in
 df = pd.DataFrame()
 
+# Loop through each state
 for state in states:
+
+    '''
+    To practice web-scraping, recommend downloading the web-page:
+    soup = BeautifulSoup(open(ufp_data.html), "html.parser")
+    '''
     
     # Get HTML from URL
-    #dat_url="http://www.nuforc.org/webreports/ndxl"+state+".html"
-    dat_url = "ufo_" + "tx" + "_data.html"
     # http://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-a-parser
-    #soup = BeautifulSoup(open(dat_url), "html.parser")     
     r = requests.get('http://www.nuforc.org/webreports/ndxl' + state + '.html')    
+
+    # Create a soup object
     soup = BeautifulSoup(r.text, "html.parser")
+    
+    # Find the table div
     table = soup.find("table", cellspacing=1)
     
-    # Initialize the dictionary
+    # Create an empty dictionary data-structure
     d = {'date_time': [],
          'city': [],
          'state': [],
@@ -42,7 +44,6 @@ for state in states:
     # Read data row by row
     for i, row in enumerate(table.findAll('tr')[1:]):
         col = row.findAll('td')
-        #print i
         d['date_time'].append(col[0].string)
         d['city'].append(col[1].string)
         d['state'].append(col[2].string)
@@ -57,11 +58,11 @@ for state in states:
     # Print message
     print state + " is complete"
 
-# Validate number of records per state
-df.state.value_counts()
-
 # Convert all states to upper case 
 df['state'] = df['state'].str.upper()
+
+# Print out number of records per state
+df.state.value_counts()
 
 # Write to csv
 df.to_csv('ufo_data.csv', index=False)
